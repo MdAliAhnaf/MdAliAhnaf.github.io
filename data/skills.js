3f42fc787f8b6184e7bbd374be87929006429e56
Md. Ali Ahnaf <aliahnaf2012@gmail.com>
2023-07-30 01:01:39 +0600

    up

--- data/skills.js
+++ data/skills.js
@@ -1,38 +1,172 @@
 export const Skills = [
 	{
 		id: 1,
+		name: 'Python',
+		logo: '/assets/images/skills/python.png',
+	},
+	{
+		id: 2,
+		name: 'C #',
+		logo: '/assets/images/skills/C_sharp.png',
+	},
+	{
+		id: 3,
+		name: 'C ++',
+		logo: '/assets/images/skills/CPlusPlus.png',
+	},
+	{
+		id: 4,
+		name: 'R',
+		logo: '/assets/images/skills/R.png',
+	},
+	{
+		id: 5,
+		name: 'PHP',
+		logo: '/assets/images/skills/PHP.png',
+	},
+	{
+		id: 6,
 		name: 'HTML5',
 		logo: '/assets/images/skills/html-5.png',
 	},
 	{
-		id: 2,
+		id: 7,
 		name: 'CSS3',
 		logo: '/assets/images/skills/css3.png',
 	},
 	{
-		id: 3,
-		name: 'Bootstrap',
-		logo: '/assets/images/skills/bootstrap.png',
+		id: 8,
+		name: 'JavaScript',
+		logo: '/assets/images/skills/JavaScript.png',
 	},
 	{
-		id: 6,
+		id: 9,
+		name: 'Java',
+		logo: '/assets/images/skills/Java.png',
+	},
+	{
+		id: 10,
+		name: 'MySql',
+		logo: '/assets/images/skills/MySql.png',
+	},
+	{
+		id: 11,
+		name: 'Oracle',
+		logo: '/assets/images/skills/Oracle.png',
+	},
+	{
+		id: 12,
+		name: 'SQLAlchemy',
+		logo: '/assets/images/skills/SQLAlchemy.png',
+	},
+	{
+		id: 13,
+		name: 'PostgreSQL',
+		logo: '/assets/images/skills/PostgreSQL.png',
+	},
+	{
+		id: 14,
+		name: 'SQLite',
+		logo: '/assets/images/skills/SQLite.png',
+	},
+	{
+		id: 15,
+		name: 'FastAPI',
+		logo: '/assets/images/skills/FastAPI.png',
+	},
+	{
+		id: 16,
+		name: 'PyTorch',
+		logo: '/assets/images/skills/PyTorch.png',
+	},
+	{
+		id: 17,
+		name: 'TensorFlow',
+		logo: '/assets/images/skills/TensorFlow.png',
+	},
+	{
+		id: 18,
+		name: '.Net',
+		logo: '/assets/images/skills/NET_core.png',
+	},
+	{
+		id: 19,
+		name: 'Bootstrap',
+		logo: '/assets/images/skills/Bootstrap.png',
+	},
+	{
+		id: 20,
+		name: 'Flask',
+		logo: '/assets/images/skills/Flask.png',
+	},
+	{
+		id: 21,
+		name: 'OpenCV',
+		logo: '/assets/images/skills/OpenCV.png',
+	},
+	{
+		id: 22,
+		name: 'Scikit-learn',
+		logo: '/assets/images/skills/scikit-learn.png',
+	},
+	{
+		id: 23,
+		name: 'NumPy',
+		logo: '/assets/images/skills/NumPy.png',
+	},
+	{
+		id: 24,
+		name: 'Pandas',
+		logo: '/assets/images/skills/Pandas.png',
+	},
+	{
+		id: 25,
+		name: 'Matplotlib',
+		logo: '/assets/images/skills/Matplotlib.png',
+	},
+	{
+		id: 26,
 		name: 'Git',
 		logo: '/assets/images/skills/git.png',
 	},
 	{
-		id: 7,
+		id: 27,
 		name: 'GitHub',
 		logo: '/assets/images/skills/github.png',
 	},
 	{
-		id: 8,
-		name: 'VS Code',
-		logo: '/assets/images/skills/vscode.png',
+		id: 28,
+		name: 'Docker',
+		logo: '/assets/images/skills/Docker.png',
 	},
 	{
-		id: 9,
-		name: 'Python',
-		logo: '/assets/images/skills/python.png',
+		id: 29,
+		name: 'AWS',
+		logo: '/assets/images/skills/AWS.png',
 	},
-	
+	{
+		id: 30,
+		name: 'Heroku',
+		logo: '/assets/images/skills/Heroku.png',
+	},
+	{
+		id: 30,
+		name: 'Azure',
+		logo: '/assets/images/skills/Azure.png',
+	},
+	{
+		id: 30,
+		name: 'Miniconda',
+		logo: '/assets/images/skills/Anaconda.png',
+	},
+	{
+		id: 31,
+		name: 'Visual Studio',
+		logo: '/assets/images/skills/Visual_Studio.png',
+	},
+	{
+		id: 32,
+		name: 'VS Code',
+		logo: '/assets/images/skills/vscode_big.png',
+	},
 ];
